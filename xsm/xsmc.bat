4233ffc5020c2b5c298648b3cd45746a6d02e2cb
gary <keijiro.im@gmail.com>
2021-09-25 16:09:10 +0900

    ver 0.19

--- xsm/xsmc.bat
+++ xsm/xsmc.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 rem Javaのパスを設定してください
-rem set PATH=C:\pleiades\java\jdk-16\bin;%PATH%
+rem set PATH=C:\pleiades\java\jdk\16\bin;%PATH%
 set CURRENT=%~dp0
 java -jar "%CURRENT%xsm.jar" -library %CURRENT%\library\ %*
 endlocal
